2750d940cfe616143fca680003d58e147cada840
Radwan-Walid <radwanwalid47@gmail.com>
2025-02-25 17:06:21 +0200

    Working Hours Bug Fix

--- src/components/landing/ContactInformation/ContactInformation.tsx
+++ src/components/landing/ContactInformation/ContactInformation.tsx
@@ -2,12 +2,20 @@
 import dayjs from "dayjs";
 import dynamic from "next/dynamic";
 // import GoogleMap from "@/components/GoogleMap/GoogleMap";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 const LeafletMap = dynamic(() => import("@/components/LeafletMap/LeafletMap"), {
   ssr: false,
 });
 
 const ContactInformation = () => {
+
+  const today = dayjs().tz("America/Edmonton").format("ddd");
+
   return (
     <section className="flex items-center gap-20 py-20 max-636:flex-col">
         <div className="flex-[0.7]">
@@ -23,14 +31,14 @@ const ContactInformation = () => {
                 <p>780-203-5047</p>
             </div>
             <div className="flex flex-col gap-1">
-                <h1 className="text-lg font-semibold">Working Hours <span className={`${ dayjs().format("ddd") === "Sun" || dayjs().format("ddd") === "Sat" ? "font-medium": "hidden" } text-sm`}>(Closed Today)</span></h1>
-                <p className={`${ dayjs().format("ddd") === "Mon" ? "font-bold": "" }`}>Mon 09:00 a.m. – 05:00 p.m.</p>
-                <p className={`${ dayjs().format("ddd") === "Tue" ? "font-bold": "" }`}>Tue 09:00 a.m. – 05:00 p.m.</p>
-                <p className={`${ dayjs().format("ddd") === "Wed" ? "font-bold": "" }`}>Wed 09:00 a.m. – 05:00 p.m.</p>
-                <p className={`${ dayjs().format("ddd") === "Thu" ? "font-bold": "" }`}>Thu 09:00 a.m. – 05:00 p.m.</p>
-                <p className={`${ dayjs().format("ddd") === "Fri" ? "font-bold": "" }`}>Fri 09:00 a.m. – 05:00 p.m.</p>
-                <p className={`${ dayjs().format("ddd") === "Sat" ? "font-bold": "" }`}>Sat Closed</p>
-                <p className={`${ dayjs().format("ddd") === "Sun" ? "font-bold": "" }`}>Sun Closed</p>
+                <h1 className="text-lg font-semibold">Working Hours <span className={`${ today === "Sun" || today === "Sat" ? "font-medium": "hidden" } text-sm`}>(Closed Today)</span></h1>
+                <p className={`${ today === "Mon" ? "font-bold": "" }`}>Mon 09:00 a.m. – 05:00 p.m.</p>
+                <p className={`${ today === "Tue" ? "font-bold": "" }`}>Tue 09:00 a.m. – 05:00 p.m.</p>
+                <p className={`${ today === "Wed" ? "font-bold": "" }`}>Wed 09:00 a.m. – 05:00 p.m.</p>
+                <p className={`${ today === "Thu" ? "font-bold": "" }`}>Thu 09:00 a.m. – 05:00 p.m.</p>
+                <p className={`${ today === "Fri" ? "font-bold": "" }`}>Fri 09:00 a.m. – 05:00 p.m.</p>
+                <p className={`${ today === "Sat" ? "font-bold": "" }`}>Sat Closed</p>
+                <p className={`${ today === "Sun" ? "font-bold": "" }`}>Sun Closed</p>
             </div>
           </div>
         </div>
